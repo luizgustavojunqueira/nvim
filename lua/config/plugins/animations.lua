218e8690daff31d94f7ff13d4da61dd97878257c
Luiz Gustavo Junqueira <luizgustavossj@gmail.com>
2025-02-17 10:59:01 -0400

    add cursor animation and fix some keymaps

---
+++ lua/config/plugins/animations.lua
@@ -0,0 +1,11 @@
+return {
+    "sphamba/smear-cursor.nvim",
+    opts = {                           -- Default  Range
+        stiffness = 0.8,               -- 0.6      [0, 1]
+        trailing_stiffness = 0.5,      -- 0.3      [0, 1]
+        distance_stop_animating = 0.5, -- 0.1      > 0
+        legacy_computing_symbols_support = true,
+        cterm_cursor_colors = { 240, 245, 250, 255 },
+        cterm_bg = 235,
+    },
+}
--- lua/config/plugins/mini_config.lua
+++ lua/config/plugins/mini_config.lua
@@ -4,6 +4,17 @@ return {
     config = function()
         require("mini.comment").setup({})
         require("mini.pairs").setup({})
-        require("mini.surround").setup({})
+        require("mini.surround").setup({
+            mappings = {
+                add = '<leader>sa',        -- Add surrounding in Normal and Visual modes
+                delete = '<leader>sd',     -- Delete surrounding
+                find = '<leader>sf',       -- Find surrounding (to the right)
+                find_left = '<leader>sF',  -- Find surrounding (to the left)
+                highlight = '<leader>sh',  -- Highlight surrounding
+                replace = '<leader>sr',    -- Replace surrounding
+                suffix_last = '<leader>l', -- Suffix to search with "prev" method
+                suffix_next = '<leader>n', -- Suffix to search with "next" method
+            }
+        })
     end
 }
